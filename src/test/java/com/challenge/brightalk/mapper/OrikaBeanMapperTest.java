fb422de63637084aa3f53f6f19ea8f94d7538c0c
Felipe Lima <felipebizz@gmail.com>
2019-05-08 13:47:52 +0100

    Unit test dor Orika Mapper

---
+++ src/test/java/com/challenge/brightalk/mapper/OrikaBeanMapperTest.java
@@ -0,0 +1,41 @@
+package com.challenge.brightalk.mapper;
+
+
+import com.challenge.brightalk.config.AppConfig;
+import com.challenge.brightalk.model.Realm;
+import com.challenge.brightalk.model.RealmDTO;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
+public class OrikaBeanMapperTest {
+
+    @Autowired
+    OrikaBeanMapper mapper;
+
+    @Test
+    public void shouldMapRealmtoRealmDto() {
+
+        // Given
+        Realm realm = new Realm();
+        realm.setKey("123");
+        realm.setName("felipe");
+        realm.setDescription("My description");
+
+        // When
+        RealmDTO realmDTO = mapper.map(realm, RealmDTO.class);
+
+        // Then
+        Assert.assertEquals(realmDTO.getKey(), "123");
+        // Our UserUserDtoMapper told orika mapper how to populate the Name attribute.
+        Assert.assertEquals(realmDTO.getName(), "felipe");
+        // Our AddressToStringConverter told orika how to convert an Description object to a String.
+        Assert.assertEquals(realmDTO.getDescription(), "My description");
+    }
+
+}
